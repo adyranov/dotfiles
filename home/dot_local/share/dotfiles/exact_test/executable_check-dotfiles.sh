1ebff94e8f3f84ee2cf3e760c04177c6a2c46ee9
Artem Dyranov <artem.dyranov@gmail.com>
2022-11-04 17:20:39 -0400

    Moved dotfiles test scripts into chezmoi root

--- home/dot_local/share/dotfiles/exact_test/executable_check-dotfiles.sh
+++ home/dot_local/share/dotfiles/exact_test/executable_check-dotfiles.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
+DIR="$(dirname "$(readlink -f "$0")")"
 
 git clone https://github.com/bats-core/bats-core.git "$DIR"/bats 2>/dev/null || true
 git clone https://github.com/bats-core/bats-assert.git "$DIR"/bats-assert 2>/dev/null || true
