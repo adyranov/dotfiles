e8373c249427855a5722a17a0998f15ac5d30dcc
Artem Dyranov <artem.dyranov@gmail.com>
2022-11-06 22:50:22 -0500

    Extracted essential packages into separate config block, extracted POSIX aliases, exports and functions

--- home/dot_local/share/dotfiles/exact_test/common-helper.bash
+++ home/dot_local/share/dotfiles/exact_test/common-helper.bash
@@ -5,11 +5,6 @@ common_setup() {
     load 'bats-assert/load.bash'
     load 'bats-file/load.bash'
 
-    [ "$(uname)" == 'Darwin' ] && OS_ID='darwin'
-
-    # shellcheck source=/dev/null disable=SC2034
-    [ "$(uname)" == 'Linux' ] && . /etc/os-release && OS_ID=$ID
-
     # shellcheck source=/dev/null
-    source "$HOME"/.config/exports.sh
+    source "$HOME"/.config/shell/exports.sh
 }
