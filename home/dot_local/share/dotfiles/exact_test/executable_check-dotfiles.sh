8c8ddd80b043437682c3fc8b36f6a72a177c0295
Artem Dyranov <1038141+adyranov@users.noreply.github.com>
2022-11-07 10:15:05 -0500

    Use ASDF to install NodeJS on Mac, shallow clone of BATS, remove .cargo and .rustup from /Users/ADyranov

--- home/dot_local/share/dotfiles/exact_test/executable_check-dotfiles.sh
+++ home/dot_local/share/dotfiles/exact_test/executable_check-dotfiles.sh
@@ -3,10 +3,10 @@ set -eo pipefail
 
 DIR="$(dirname "$(readlink -f "$0")")"
 
-git clone https://github.com/bats-core/bats-core.git "$DIR"/bats 2>/dev/null || true
-git clone https://github.com/bats-core/bats-assert.git "$DIR"/bats-assert 2>/dev/null || true
-git clone https://github.com/bats-core/bats-support.git "$DIR"/bats-support 2>/dev/null || true
-git clone https://github.com/bats-core/bats-file.git "$DIR"/bats-file 2>/dev/null || true
+git clone --depth=1 https://github.com/bats-core/bats-core.git "$DIR"/bats 2>/dev/null || true
+git clone --depth=1 https://github.com/bats-core/bats-assert.git "$DIR"/bats-assert 2>/dev/null || true
+git clone --depth=1 https://github.com/bats-core/bats-support.git "$DIR"/bats-support 2>/dev/null || true
+git clone --depth=1 https://github.com/bats-core/bats-file.git "$DIR"/bats-file 2>/dev/null || true
 
 "${DIR}"/bats/bin/bats "${DIR}"/*.bats
 
