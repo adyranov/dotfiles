23a988f1da97cdf2c4e6b5adbe9dbaf0eb61e384
Artem Dyranov <1038141+adyranov@users.noreply.github.com>
2025-10-02 23:13:38 -0400

    Replaced asdf and aqua with mise

--- home/dot_local/share/dotfiles/exact_test/common-helper.bash
+++ home/dot_local/share/dotfiles/exact_test/common-helper.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 common_setup() {
-    bats_require_minimum_version 1.8.0
+    bats_require_minimum_version 1.12.0
     load 'bats-support/load.bash'
     load 'bats-assert/load.bash'
     load 'bats-file/load.bash'
